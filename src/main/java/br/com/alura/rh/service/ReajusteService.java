5cd95a6597a62fe197e386f463c7e1a723ae2c6e
Winston Spencer Sonnesen <sonnesen@gmail.com>
2021-02-28 21:58:37 -0300

    Curso Solid com Java: Princípios da programação orientada a objetos - Aula 2

---
+++ src/main/java/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,23 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.stereotype.Service;
+
+import br.com.alura.rh.model.Funcionario;
+import br.com.alura.rh.model.exception.ValidacaoException;
+
+@Service
+public class ReajusteService {
+
+	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+		BigDecimal salarioAtual = funcionario.getSalario();
+		BigDecimal percentualReajuste = aumento.divide(salarioAtual , RoundingMode.HALF_UP);
+		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+		}
+		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		funcionario.atualizarSalario(salarioReajustado);
+	}
+}
